1929208f32a0d0739dfaade4579cf99f30574fde
Subhan Khan <k.subhan9090@gmail.com>
2019-10-22 01:44:27 +0500

    Register,Login,Splash

--- app/src/main/java/com/octalabs/challetapp/activities/ActivityLogin.java
+++ app/src/main/java/com/octalabs/challetapp/activities/ActivityLogin.java
@@ -1,16 +1,28 @@
 package com.octalabs.challetapp.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.octalabs.challetapp.R;
 
 public class ActivityLogin extends Activity {
 
 
+    private Button mBtnSignUP;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mBtnSignUP = findViewById(R.id.btn_sig_up);
+        mBtnSignUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivityLogin.this,RegisterActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/octalabs/challetapp/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.octalabs.challetapp.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.octalabs.challetapp.R;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
